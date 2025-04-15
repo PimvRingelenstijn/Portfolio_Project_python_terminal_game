93aaa7a4d4039a4d5f3b74da316306aea401b62f
PimvRingelenstijn <pimvringelenstijn@gmail.com>
2025-04-15 14:55:26 +0200

    Add blackjack file & open card_info file

--- blackjack.py
+++ blackjack.py
@@ -1,3 +1,14 @@
-x = 1
-y = 2
-z = x * y
+import csv
+
+# create empty dictionary to read into
+card_deck = {}
+
+# open card info file and turn it into usable dictionary
+with open('card_information.csv') as card_info_file:
+    reader = csv.reader(card_info_file)
+    next(reader) #skip header
+    for row in reader:
+        card, value = row #card = row[0], value = row[1]
+        card_values[card] = int(value)  
+
+print(card_deck)
